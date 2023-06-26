bcb079634cc7344817f1923c3f96c5d7ea217f0d
annamaria <szumiecam@gmail.com>
2023-06-26 18:34:16 +0200

    Added Movie Card Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @NgModule({
-  declarations: [AppComponent, UserRegistrationFormComponent],
+  declarations: [
+    AppComponent,
+    UserRegistrationFormComponent,
+    MovieCardComponent,
+    UserLoginFormComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
